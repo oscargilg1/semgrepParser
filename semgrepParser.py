d3cfad56a0a67a803a54dca45d7bbda4522af6e2
oscargilg1 <69176803+oscargilg1@users.noreply.github.com>
2023-02-26 16:18:25 -0500

    Update semgrepParser.py

--- semgrepParser.py
+++ semgrepParser.py
@@ -9,8 +9,9 @@ issues = []
 for issue in data['results']:
     if issue['extra'].get('impact') == 'HIGH' or issue['extra'].get('severity') == 'ERROR' or issue['extra'].get('severity') == 'WARNING':
         message = issue['extra'].get('message') or issue['check_name']
-        issues.append({'check_id': issue['check_id'], 'path': issue['path'], 'message': message})
+        codeLine = issue['start'].get('line')
+        issues.append({'check_id': issue['check_id'], 'path': issue['path'], 'message': message, 'line': codeLine})
 
-df = pd.DataFrame(issues, columns=['check_id', 'path', 'message'])
+df = pd.DataFrame(issues, columns=['check_id', 'path', 'message', 'line'])
 html = df.to_html(index=False)
 print(html)
